57287ad2f615a736f9ccbec214e29ae8da92565d
Sheena Artrip <sheena.artrip@gmail.com>
2016-05-07 04:50:16 -0400

    nixos - make r8168 driver kernel 4.5 compatible

--- nixos/r8168.nix
+++ nixos/r8168.nix
@@ -1,33 +1,19 @@
-{ stdenv, fetchurl, utillinux, nukeReferences
-, configFile ? "all"
-
-# Kernel dependencies
-, kernel ? null, spl ? null
-}:
+{ stdenv, fetchurl, nukeReferences, kernel ? null }:
 
 with stdenv.lib;
-let
-  buildKernel = any (n: n == configFile) [ "kernel" "all" ];
-  buildUser = any (n: n == configFile) [ "user" "all" ];
-in
-
-assert any (n: n == configFile) [ "kernel" "user" "all" ];
-assert buildKernel -> kernel != null && spl != null;
 
 stdenv.mkDerivation rec {
-  name = "r8168-${configFile}-${version}${optionalString buildKernel "-${kernel.version}"}";
-
+  name = "r8168-${version}-${kernel.version}";
   version = "8.041.01";
 
+  # NOTE: there is an unnofficial github mirror for the source as well
   src = fetchurl {
     url = "http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/cn/nic/0004-r8168-8.041.01.tar.bz2";
     sha256 = "0qpc72svjcnc7i812px4b8z1jd1xjal3q4v8xa3z60nr3l2d45vh";
   };
 
-  # for zdb to get the rpath to libgcc_s, needed for pthread_cancel to work
-  #NIX_CFLAGS_LINK = "-lgcc_s";
-
   preBuild = ''
+    ${optionalString (versionAtLeast kernel.version "4.5") "patch -p1 < ${./4.5.patch}"}
     substituteInPlace src/Makefile \
       --replace "\$(shell uname -r)" "${kernel.modDirVersion}" \
       --replace "/lib/modules" "${kernel.dev}/lib/modules" \
@@ -40,20 +26,24 @@ stdenv.mkDerivation rec {
       install -v -D -m 644 src/r8168.ko "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/"
   '';
 
+  dontStrip = true;
+
   enableParallelBuilding = true;
 
   postInstall = ''
     # Prevent kernel modules from depending on the Linux -dev output.
     nuke-refs $(find $out -name "*.ko")
-  '' + optionalString buildUser ''
-
   '';
 
   meta = {
-    description = "Realtek r8168 Linux Kernel module";
+    description = "Linux kernel module for Realtek Gigabit Ethernet controllers";
     longDescription = ''
+        Linux device driver released for RealTek RTL8168B/8111B, RTL8168C/8111C,
+        RTL8168CP/8111CP, RTL8168D/8111D, RTL8168DP/8111DP, and RTL8168E/8111E
+        Gigabit Ethernet controllers with PCI-Express interface.
       '';
-    platforms = platforms.linux;
+    platforms = [ "x86_64-linux" "i686-linux" ];
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ sheenobu ];
   };
 }
