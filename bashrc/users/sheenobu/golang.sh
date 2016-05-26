0aef815af98f9e170520eda8d4fbaaa7c59802c8
Sheena Artrip <sheena.artrip@gmail.com>
2016-05-26 04:00:07 -0400

    bashrc - added GOPATH switching

--- bashrc/users/sheenobu/golang.sh
+++ bashrc/users/sheenobu/golang.sh
@@ -1,8 +1,14 @@
-export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:~/go-bin"
 
-GOPATH=~/go
-export GOPATH
-
-PATH="$PATH:$GOPATH/bin"
-
-export PATH
+function go-switch-path {
+	GOROOT=`nix-env -q --out-path go | awk '{print $2}'`/share/go/
+	GOPATH=`readlink -f $1`
+	GONAME=`basename $1`
+	rm -f ~/go-bin
+	ln -sf $GOPATH/bin ~/go-bin
+	echo "GOPATH=$GOPATH"
+	echo "GOROOT=$GOROOT"
+	export GOPATH
+	export GONAME
+	export GOROOT
+}
