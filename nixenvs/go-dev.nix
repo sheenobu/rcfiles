1d296258091f7ee99743f6c854572ffcbf5fb336
Sheena Artrip <sheena.artrip@gmail.com>
2016-06-21 00:32:31 -0400

    nixenvs - ensure go 1.6 in go-dev

--- nixenvs/go-dev.nix
+++ nixenvs/go-dev.nix
@@ -1,10 +1,10 @@
 with import <nixpkgs> {}; {
   goEnv = stdenv.mkDerivation {
-    name = "go-main";
-    buildInputs = [ stdenv bash go ];
+    name = "go-1.6-main";
+    buildInputs = [ stdenv bash go_1_6 ];
 
 	shellHook = ''
-		export GOROOT=${go}/share/go
+		export GOROOT=${go_1_6}/share/go
 		export GOPATH=/home/sheenobu/go-paths/go-main
 		export PATH="$PATH:$GOPATH/bin"
 	'';
